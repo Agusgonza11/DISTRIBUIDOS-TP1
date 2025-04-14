ac4e1116eec16faf614343c79fdea1222388b44c
Agustin Gonzalez <aguselgonza@gmail.com>
2025-04-13 23:56:30 -0300

    modularizo envio de mensajes

---
+++ server/workers/communication.py
@@ -0,0 +1,44 @@
+import aio_pika
+import logging
+from common.utils import esperar_conexion
+
+
+conexion = None
+canal = None
+
+# ---------------------
+# GENERALES
+# ---------------------
+async def inicializar_comunicacion():
+    global conexion, canal
+    conexion = await esperar_conexion()
+    canal = await conexion.channel()
+    await canal.set_qos(prefetch_count=1)
+
+
+async def enviar_mensaje(routing_key, body):
+    await canal.default_exchange.publish(
+        aio_pika.Message(body=body.encode()),
+        routing_key=routing_key
+    )
+
+
+# ---------------------
+# FILTRO
+# ---------------------
+async def escuchar_colas_para_filtro(callback_filtro):
+    for consulta_id in range(1, 6):
+        nombre_entrada = f"filter_consult_{consulta_id}"
+        nombre_salida = f"gateway_output_{consulta_id}" if consulta_id == 1 else f"aggregator_consult_{consulta_id}"
+
+        await canal.declare_queue(nombre_entrada, durable=True)
+        await canal.declare_queue(nombre_salida, durable=True)
+
+        async def wrapper(mensaje, consulta_id=consulta_id):
+            async with mensaje.process():
+                contenido = mensaje.body.decode('utf-8') 
+                await callback_filtro(consulta_id, contenido, enviar_mensaje)
+
+        queue = await canal.get_queue(nombre_entrada)
+        await queue.consume(wrapper)
+        logging.info(f"Escuchando en {nombre_entrada}")
--- server/workers/filter.py
+++ server/workers/filter.py
@@ -1,8 +1,9 @@
 import asyncio
-import aio_pika
-import json
 import logging
-from common.utils import initialize_log, esperar_conexion
+from common.utils import initialize_log
+from workers.test import enviar_mock
+from workers.communication import inicializar_comunicacion, escuchar_colas_para_filtro
+
 
 # -----------------------
 # Nodo Filtro
@@ -16,15 +17,15 @@ class FiltroNode:
         
         match consulta_id:
             case 1:
-                return self.consulta_1(lineas)
+                return self.consulta_1(datos)
             case 2:
-                return self.consulta_2(lineas)
+                return self.consulta_2(datos)
             case 3:
-                return self.consulta_3(lineas)
+                return self.consulta_3(datos)
             case 4:
-                return self.consulta_4(lineas)
+                return self.consulta_4(datos)
             case 5:
-                return self.consulta_5(lineas)
+                return self.consulta_5(datos)
             case _:
                 logging.warning(f"Consulta desconocida: {consulta_id}")
                 return []
@@ -57,55 +58,21 @@ class FiltroNode:
 
 filtro = FiltroNode()
 
-async def procesar_mensaje(mensaje: aio_pika.IncomingMessage, consulta_id: int):
-    async with mensaje.process():
-        try:
-            batch = mensaje.body.decode() #Me llega el batch
-            if batch.strip() == "EOF":
-                logging.info(f"Consulta {consulta_id} recibió EOF")
-                return
-            
-            resultado = filtro.ejecutar_consulta(consulta_id, batch)
+async def callback_filtro(consulta_id, contenido, enviar_func):
+    if contenido.strip() == b"EOF":
+        logging.info(f"Consulta {consulta_id} recibió EOF")
+        return
+    resultado = filtro.ejecutar_consulta(consulta_id, contenido)
+    destino = f"gateway_output_{consulta_id}" if consulta_id == 1 else f"aggregator_consult_{consulta_id}"
+    await enviar_func(destino, resultado)
 
-            # Publicamos el resultado en una cola única por consulta (opcional)
-            if consulta_id == 1:
-                await mensaje.channel.default_exchange.publish(
-                    aio_pika.Message(body="\n".join(resultado).encode()),
-                    routing_key=f"gateway_output_{consulta_id}"
-                )
-            else:
-                await mensaje.channel.default_exchange.publish(
-                    aio_pika.Message(body="\n".join(resultado).encode()),
-                    routing_key=f"aggregator_consult_{consulta_id}"
-                )
-        except Exception as e:
-            print(f"Error procesando mensaje en consulta {consulta_id}: {e}")
 
 async def main():
     initialize_log("INFO")
     logging.info("Se inicializó el worker filter")
-    conexion = await esperar_conexion()
-    canal = await conexion.channel()
-    await canal.set_qos(prefetch_count=1)
-
-    # Crear y consumir desde 5 colas diferentes
-    for consulta_id in range(1, 6):
-        queue_name = f"filter_consult_{consulta_id}"
-        await canal.declare_queue(queue_name, durable=True)
-        if consulta_id == 1:
-            await canal.declare_queue(f"gateway_output_{consulta_id}", durable=True)
-        else:
-            await canal.declare_queue(f"aggregator_consult_{consulta_id}", durable=True)
-
-        # Creamos una función wrapper con el número de consulta ya definido
-        async def callback_wrapper(msg, consulta_id=consulta_id):
-            await procesar_mensaje(msg, consulta_id)
-
-        await canal.set_qos(prefetch_count=1)
-        queue = await canal.get_queue(queue_name)
-        await queue.consume(callback_wrapper)
-        logging.info(f"Filtro escuchando en {queue_name}")
-
+    await inicializar_comunicacion()
+    await escuchar_colas_para_filtro(callback_filtro)
+    await enviar_mock()
     await asyncio.Future()
 
 asyncio.run(main())
